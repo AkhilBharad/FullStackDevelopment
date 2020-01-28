1312d1310d6bbf7d596cedce1b9f70937e3f0461
Akhil Bharadwaj <bharadwaj16akhil@gmail.com>
2020-01-28 04:41:33 -0800

    jan 28

---
+++ Jan 28/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './user/register/register.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
+import { UserService } from './services/user.service';
+import { UsersComponent } from './users/users.component';
+
+import{HttpClientModule} from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
+import { LoginComponent } from './login/login.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    
+    UsersComponent,
+    
+    AddUserComponent,
+    
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
